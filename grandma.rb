d238f16d652472dd52c2b2eb1d77edc6e61b4e2e
Iskra B <ibatistapoblete@gmail.com>
2019-04-26 02:11:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else phrase
+  else phrase != phrase.upcase
     return
     "HUH?! SPEAK UP, SONNY!"
   end
